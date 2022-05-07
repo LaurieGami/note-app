6e807e4b51ef7f436da28e7ecf49fb1aa41f8923
lauriegami <laurievilleton@gmail.com>
2022-05-07 12:00:37 -0700

    Added a form to create a note and add it to a list of notes being displ ayed, as well as a delete function to remove the note from the list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
+import styled from 'styled-components'
 
 const Container = styled.div`
   display: flex;
@@ -13,10 +15,55 @@ const Title = styled.h1`
   color: red;
 `
 
+const Button = styled.button`
+  color: blue;
+`
+
 function App() {
+  const [notes, setNotes] = useState([])
+  const [draft, setDraft] = useState('')
+
+  function handleChange(e) {
+    setDraft(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const note = { id: uuid(), value: draft, createdAt: new Date() }
+    setNotes([...notes, note])
+    setDraft('')
+  }
+
+  function onDelete(id) {
+    const newNotes = notes.filter(note => note.id !== id)
+    setNotes(newNotes)
+  }
+
   return (
     <Container>
-      <Title>Hello World!</Title>
+      <Title>Note App</Title>
+      <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="comment">Note</label>
+            <textarea
+              placeholder="Write your note here"
+              value={draft}
+              onChange={handleChange}
+            />
+        </div>
+        <Button type="submit">Add note</Button>
+      </form>
+      <div>
+        {notes.map((note) => {
+          return (
+            <div key={note.id}>
+              <h3>{note.createdAt.toString()}</h3>
+              <p>{note.value}</p>
+              <button onClick={() => onDelete(note.id)}>Delete</button>
+            </div>
+          )
+        })}
+      </div>
     </Container>
   );
 }
