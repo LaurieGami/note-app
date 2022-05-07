53d802c7e265138bd2131750760c420f2a00bc51
lauriegami <laurievilleton@gmail.com>
2022-05-07 12:48:04 -0700

    Ran prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react'
-import { v4 as uuid } from 'uuid'
-import styled from 'styled-components'
-import Note from './components/Note'
+import { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import styled from "styled-components";
+import Note from "./components/Note";
 
 const Container = styled.div`
   display: flex;
@@ -10,45 +10,47 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 100vh;
-`
+`;
 
 const Title = styled.h1`
   color: red;
-`
+`;
 
 const Button = styled.button`
   color: blue;
-`
+`;
 
 function App() {
-  const [notes, setNotes] = useState(JSON.parse(window.localStorage.getItem('notes')) || [])
-  const [draft, setDraft] = useState('')
+  const [notes, setNotes] = useState(
+    JSON.parse(window.localStorage.getItem("notes")) || []
+  );
+  const [draft, setDraft] = useState("");
 
   useEffect(() => {
-    window.localStorage.setItem('notes', JSON.stringify(notes))
-  }, [notes])
+    window.localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   function handleChange(e) {
-    setDraft(e.target.value)
+    setDraft(e.target.value);
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
-    const note = { id: uuid(), value: draft, createdAt: new Date() }
-    setNotes([...notes, note])
-    setDraft('')
+    e.preventDefault();
+    const note = { id: uuid(), value: draft, createdAt: new Date() };
+    setNotes([...notes, note]);
+    setDraft("");
   }
 
   function onDelete(id) {
-    const newNotes = notes.filter(note => note.id !== id)
-    setNotes(newNotes)
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
   }
 
   function onSave(id, value) {
-    const noteIndex = notes.findIndex(note => note.id === id)
-    const newNotes = [...notes]
-    newNotes[noteIndex].value = value
-    setNotes(newNotes)
+    const noteIndex = notes.findIndex((note) => note.id === id);
+    const newNotes = [...notes];
+    newNotes[noteIndex].value = value;
+    setNotes(newNotes);
   }
 
   return (
@@ -56,12 +58,12 @@ function App() {
       <Title>Note App</Title>
       <form onSubmit={handleSubmit}>
         <div>
-            <label htmlFor="comment">Note</label>
-            <textarea
-              placeholder="Write your note here"
-              value={draft}
-              onChange={handleChange}
-            />
+          <label htmlFor="comment">Note</label>
+          <textarea
+            placeholder="Write your note here"
+            value={draft}
+            onChange={handleChange}
+          />
         </div>
         <Button type="submit">Add note</Button>
       </form>
@@ -74,7 +76,7 @@ function App() {
               onDelete={onDelete}
               onSave={onSave}
             />
-          )
+          );
         })}
       </div>
     </Container>
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,32 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function Note({ note, onDelete, onSave }) {
-  const [text, setText] = useState(note.value)
-  const [edit, setEdit] = useState(false)
+  const [text, setText] = useState(note.value);
+  const [edit, setEdit] = useState(false);
 
   function handleSave() {
-    onSave(note.id, text)
-    setEdit(!edit)
+    onSave(note.id, text);
+    setEdit(!edit);
   }
 
   return (
     <div>
-        {edit ? <button onClick={() => handleSave()}>Save</button> : <button onClick={() => setEdit(!edit)}>Edit</button>}
-        <button onClick={() => onDelete(note.id)}>Delete</button>
-        {edit ? (
-          <textarea
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-        ) : (
-          <>
-            <h2>{new Date(note.createdAt).toString()}</h2>
-            <p>{text}</p>
-          </>
-        )
-      }
+      {edit ? (
+        <button onClick={() => handleSave()}>Save</button>
+      ) : (
+        <button onClick={() => setEdit(!edit)}>Edit</button>
+      )}
+      <button onClick={() => onDelete(note.id)}>Delete</button>
+      {edit ? (
+        <textarea value={text} onChange={(e) => setText(e.target.value)} />
+      ) : (
+        <>
+          <h2>{new Date(note.createdAt).toString()}</h2>
+          <p>{text}</p>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default Note;
